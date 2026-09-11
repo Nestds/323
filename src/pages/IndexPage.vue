dad40219543ab8bafd015bd464674afc5c44ff54
Nestds <143984411+Nestds@users.noreply.github.com>
2026-09-11 14:09:56 +0700

    v.5-Accept with Chinese language-323

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -21,9 +21,9 @@
       />
 
       <q-toggle
-        v-model="accept"
-        label="I accept the license and terms"
-      />
+  v-model="accept"
+  label="I accept the license and terms (我接受许可和条款语言)"
+/>
 
       <div>
         <q-btn
